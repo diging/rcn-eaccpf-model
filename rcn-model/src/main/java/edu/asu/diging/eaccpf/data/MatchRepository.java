460fbd04d4ab014dced07e6c88f5669a62699342
Julia Damerow <julia.damerow@gmail.com>
2019-10-29 15:57:12 -0400

    queries for showing matches

--- rcn-model/src/main/java/edu/asu/diging/eaccpf/data/MatchRepository.java
+++ rcn-model/src/main/java/edu/asu/diging/eaccpf/data/MatchRepository.java
@@ -1,9 +1,24 @@
 package edu.asu.diging.eaccpf.data;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import edu.asu.diging.eaccpf.model.match.impl.MatchImpl;
 
 public interface MatchRepository extends PagingAndSortingRepository<MatchImpl, String>  {
 
+    List<MatchImpl> findByJobId(String jobId, Pageable pageable);
+    
+    @Query("select distinct m.baseRecordId from MatchImpl m where m.jobId = :jobId")
+    List<Object[]> getMatchedRecordIds(@Param("jobId") String jobId, Pageable pageable);
+    
+    @Query("select count(distinct m.baseRecordId) from MatchImpl m where m.jobId = :jobId")
+    Integer countMatchedRecordIds(@Param("jobId") String jobId);
+    
+    MatchImpl findFirstByBaseRecordIdOrderByOverallScoreDesc(String baseRecordId);
 }
+
